fff1d0ca7cbd56d2b92bcd049429aab5c919e436
Gillan0 <antonino.gillard@orange.fr>
2026-02-17 14:16:49 +0100

    lab1-2: save resnet model generator

---
+++ src/prez1/debug_dataloader.py
@@ -0,0 +1,28 @@
+from torchvision.datasets import CIFAR10
+import numpy as np
+import torchvision.transforms as transforms
+import torch
+from torch.utils.data.dataloader import DataLoader
+
+from dataloader import c10train
+
+## number of target samples for the final dataset
+num_train_examples = len(c10train)
+num_samples_subset = 15000
+
+## We set a seed manually so as to reproduce the results easily
+seed  = 2147483647
+
+## Generate a list of shuffled indices ; with the fixed seed, the permutation will always be the same, for reproducibility
+indices = list(range(num_train_examples))
+np.random.RandomState(seed=seed).shuffle(indices)## modifies the list in place
+
+## We define the Subset using the generated indices
+c10train_subset = torch.utils.data.Subset(c10train,indices[:num_samples_subset])
+print(f"Initial CIFAR10 dataset has {len(c10train)} samples")
+print(f"Subset of CIFAR10 dataset has {len(c10train_subset)} samples")
+
+# Finally we can define anoter dataloader for the training data
+trainloader_subset = DataLoader(c10train_subset,batch_size=32,shuffle=True)
+### You can now use either trainloader (full CIFAR10) or trainloader_subset (subset of CIFAR10) to train your networks.
+trainloader = trainloader_subset
---
+++ src/prez1/main.py
@@ -0,0 +1,160 @@
+'''Train CIFAR10 with PyTorch.'''
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+
+import torchvision
+import torchvision.transforms as transforms
+
+import os
+import argparse
+
+from resnet import *
+from utils import progress_bar
+
+from dataloader import testloader, trainloader
+#from debug_dataloader import trainloader
+
+
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--lr', default=0.001, type=float, help='learning rate')
+parser.add_argument('--resume', '-r', action='store_true',
+                    help='resume from checkpoint')
+parser.add_argument('--name', default="ckpt.pth", type=str, help='Model ID')
+parser.add_argument('--log', default='training.log', type=str, help='Log file name (.log)')
+parser.add_argument('--epoch', default=50, type=int, help='Number of epochs')
+parser.add_argument('--model', default="cosine", type=str, help="Model of choice : 'cosine', 'plateau', 'adam'")
+
+args = parser.parse_args()
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+best_acc = 0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+NB_EPOCH = args.epoch
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck')
+
+# Model
+print('==> Building model..')
+net = ResNet18()
+net = net.to(device)
+if device == 'cuda':
+    net = torch.nn.DataParallel(net)
+    cudnn.benchmark = True
+
+if args.resume:
+    # Load checkpoint.
+    print('==> Resuming from checkpoint..')
+    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+    checkpoint = torch.load('./checkpoint/ckpt.pth')
+    net.load_state_dict(checkpoint['net'])
+    best_acc = checkpoint['acc']
+    start_epoch = checkpoint['epoch']
+"""
+else:
+    time.
+"""
+criterion = nn.CrossEntropyLoss()
+
+if args.model == "adam":
+    optimizer = optim.Adam(net.parameters(), 
+                       lr=args.lr, 
+                       weight_decay=5e-6)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=5)
+
+elif args.model == "plateau":
+    optimizer = optim.SGD(net.parameters(), lr=args.lr,
+                        momentum=0.9, weight_decay=5e-6)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=5)
+
+else:
+    optimizer = optim.SGD(net.parameters(), lr=args.lr,
+                      momentum=0.9, weight_decay=5e-6)        
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=NB_EPOCH)
+
+# Log file 
+# Initialize log file
+with open("./logs/" + args.log, "w") as f:
+    f.write("epoch,train_loss,train_acc,test_loss,test_acc\n")
+
+
+# Training
+def train(epoch):
+    print('\nEpoch: %d' % epoch)
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                    % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+    avg_loss = train_loss / len(trainloader)
+    acc = 100. * correct / total
+
+    return avg_loss, acc
+
+def test(epoch):
+    global best_acc
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(testloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+            # progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+            #             % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+    avg_loss = test_loss / len(testloader)
+    acc = 100. * correct / total
+
+    # Save checkpoint.
+    acc = 100.*correct/total
+    if acc > best_acc:
+        print('Saving..')
+        state = {
+            'net': net.state_dict(),
+            'acc': acc,
+            'epoch': epoch,
+        }
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+        torch.save(state, './checkpoint/' + args.name)
+        best_acc = acc
+
+    return avg_loss, acc
+
+for epoch in range(start_epoch, start_epoch + NB_EPOCH):
+    train_loss, train_acc = train(epoch)
+    test_loss, test_acc = test(epoch)
+    if args.model == "cosine":
+        scheduler.step()
+    else:
+        scheduler.step(train_loss)
+
+    with open("./logs/" + args.log, "a") as f:
+        f.write(f"{epoch},{train_loss:.4f},{train_acc:.2f},{test_loss:.4f},{test_acc:.2f}\n")
